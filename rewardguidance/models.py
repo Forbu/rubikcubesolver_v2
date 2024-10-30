d6629a82bd028bc8d21cca46dd395a7b9f374396
Adrien Bufort <adrien.bufort@orange.com>
2024-10-30 17:17:34 +0100

    update model without self consistency

--- rewardguidance/models.py
+++ rewardguidance/models.py
@@ -18,7 +18,15 @@ from rewardguidance.mamba2 import Mamba2, Mamba2Config
 
 class RewardGuidanceModel(nn.Module):
     """
-    Reward guidance model.
+    Reward guidance model with a time flags and a shortcut value.
+
+    Shortcut value is used to indicate if the trajectory is a shortcut or not.
+    Time flags is used to indicate the time of the trajectory.
+
+    For shortcut value see paper : https://arxiv.org/pdf/2410.12557
+
+    Time is the classical time value in diffusion models.
+
     """
 
     def __init__(
@@ -59,32 +67,51 @@ class RewardGuidanceModel(nn.Module):
         # embedding for the time flags
         self.time_flags_embedding = nn.Linear(1, nb_hidden_dim * 4)
 
+        # embedding for the shortcut value
+        self.shortcut_value_embedding = nn.Linear(1, nb_hidden_dim * 4)
+
+        # linear layer for reward
+        self.reward_layer = nn.Linear(nb_hidden_dim, 1)
+
     def forward(
         self,
         init_states: torch.Tensor,
         future_states: torch.Tensor,
         time_flags: torch.Tensor,
+        shortcut_value: float = 0.0,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Args:
-            init_states: (batch_size, seq_len, dim)
-            future_states: (batch_size, seq_len, dim)
-            time_flags: (batch_size, 1) value between 0 and 1
+            init_states: (batch_size, seq_len, 6*9, 6)
+            future_states: (batch_size, seq_len, 6*9, 6)
+            time_flags: (batch_size, 1) value between 0 and 1 (for the time of the trajectory)
+            shortcut_value: (batch_size, 1) value between 0 and 1 (for the shortcut value)
         Returns:
             (batch_size, seq_len, nb_output_dim)
         """
         # we retrieve the dimensions of the inputs (batch_size, seq_len, dim)
-        batch_size, seq_init_states, _ = init_states.shape
-        batch_size, seq_future_states, _ = future_states.shape
+        batch_size, seq_init_states, _, _ = init_states.shape
+        batch_size, seq_future_states, _, _ = future_states.shape
 
-        assert seq_init_states == self.nb_init_states
-        assert seq_future_states == self.nb_future_states
+        assert (
+            seq_init_states == self.nb_init_states
+        ), f"seq_init_states {seq_init_states} != nb_init_states {self.nb_init_states}"
+        assert (
+            seq_future_states == self.nb_future_states
+        ), f"seq_future_states {seq_future_states} != nb_future_states {self.nb_future_states}"
+
+        # first we flatten the future states and the init states (for the last two dimensions)
+        init_states = init_states.view(batch_size, seq_init_states, -1)
+        future_states = future_states.view(batch_size, seq_future_states, -1)
 
         # we create the embeddings for the future states and the init states
         states_embeddings = self.states_embedding.repeat(batch_size, 1, 1)
 
         # we create the embeddings for the time flags
         time_flags_embeddings = self.time_flags_embedding(time_flags).unsqueeze(1)
+        shortcut_value_embeddings = self.shortcut_value_embedding(
+            shortcut_value
+        ).unsqueeze(1)
 
         # we concatenate the init states and the future states embeddings
         x = torch.cat([init_states, future_states], dim=1)
@@ -97,8 +124,13 @@ class RewardGuidanceModel(nn.Module):
 
         # modification of the input to add the time flags embeddings [:, :, :self.nb_hidden_dim]
         x = (
-            x * time_flags_embeddings[:, :, : self.nb_hidden_dim]
+            x
+            * time_flags_embeddings[:, :, : self.nb_hidden_dim]
+            * shortcut_value_embeddings[:, :, : self.nb_hidden_dim]
             + time_flags_embeddings[:, :, self.nb_hidden_dim : (self.nb_hidden_dim * 2)]
+            + shortcut_value_embeddings[
+                :, :, self.nb_hidden_dim : (self.nb_hidden_dim * 2)
+            ]
         )
 
         # we pass the input through the mamba2 model
@@ -110,10 +142,23 @@ class RewardGuidanceModel(nn.Module):
             * time_flags_embeddings[
                 :, :, (self.nb_hidden_dim * 2) : (self.nb_hidden_dim * 3)
             ]
+            * shortcut_value_embeddings[
+                :, :, (self.nb_hidden_dim * 2) : (self.nb_hidden_dim * 3)
+            ]
             + time_flags_embeddings[:, :, (self.nb_hidden_dim * 3) :]
+            + shortcut_value_embeddings[:, :, (self.nb_hidden_dim * 3) :]
         )
 
         # we pass the output through the output layer
-        x = self.output_layer(x)
+        state_final = self.output_layer(x)
 
-        return x
+        # # we pass the output through the reward layer
+        # reward = self.reward_layer(state_final)
+
+        # # sum for the reward over the time
+        # reward = reward.max(dim=1)
+
+        # we want to modify the state_final to be of shape (batch_size, seq_len, 6*9, 6)
+        state_final = state_final.view(batch_size, seq_future_states, -1, 6)
+
+        return state_final #, reward
--- rewardguidance/pl_models.py
+++ rewardguidance/pl_models.py
@@ -10,22 +10,89 @@ import lightning as L
 import torch
 import torch.nn.functional as F
 
+from torch.distributions.dirichlet import Dirichlet
+
+
 class RewardGuidanceModel(L.LightningModule):
     def __init__(self, model: torch.nn.Module, lr: float = 1e-3):
         super().__init__()
         self.model = model
         self.lr = lr
 
-    def forward(self, init_states: torch.Tensor, future_states: torch.Tensor, time_flags: torch.Tensor) -> torch.Tensor:
+        # dirichlet distribution parameter
+        self.dirichlet_alpha = 0.5
+        self.nb_dim_dirichlet = 6
+
+        self.dirichlet_dist = Dirichlet(
+            torch.tensor([self.dirichlet_alpha] * self.nb_dim_dirichlet)
+        )
+
+    def forward(
+        self,
+        init_states: torch.Tensor,
+        future_states: torch.Tensor,
+        time_flags: torch.Tensor,
+    ) -> torch.Tensor:
         return self.model(init_states, future_states, time_flags)
 
     def training_step(self, batch, batch_idx):
-        init_states, future_states, time_flags, rewards = batch
-        pass
-    
-    def validation_step(self, batch, batch_idx):
-        init_states, future_states, time_flags, rewards = batch
-        pass
+        """
+        Training step for the reward guidance model.
+
+        We will use a matching flow setup to train the model.
+
+
+        We will use a time flags and a shortcut value to enable fast sampling.
+
+
+        In batch we have init_states, future_states and rewards.
+            init_states is of shape (batch_size, nb_init_seq, 9*6, 6)
+            future_states is of shape (batch_size, nb_future_seq, 9*6, 6)
+            rewards is of shape (batch_size, 1)
+        """
+        init_states, future_states, rewards = batch
+
+        batch_size = init_states.shape[0]
+
+        # here we should generate a time flags
+        time_flags = torch.rand(batch_size, 1)
+
+        # here we should generate a shortcut value but for the first batch_size * 3 / 4
+        # we set it to 0 and the rest random between 0 and 1
+        shortcut_value = torch.zeros(batch_size, 1)
+
+        # create a noisy future_states (matching flow setup)
+        pur_noise = self.dirichlet_dist.sample(
+            sample_shape=(batch_size, future_states.shape[1], 64)
+        )
+        future_states_noisy = future_states * time_flags + pur_noise * (
+            1.0 - time_flags
+        )
+
+        # forward pass
+        state_final_logits = self.forward(
+            init_states, future_states_noisy, time_flags, shortcut_value
+        )
+
+        target = future_states - pur_noise
+
+        output_target = torch.zeros_like(target)
+
+        # compute the loss for the reward and the state_final_logits for
+        # the shortcut value 0
+        output_target = (
+            torch.softmax(
+                state_final_logits, dim=-1
+            )
+            - 1.0 / self.nb_dim_dirichlet
+        )
+
+        loss_regularization = F.mse_loss(output_target, target)
+
+        # log the loss
+        self.log("train_loss", loss_regularization)
+
+        return loss_regularization
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
